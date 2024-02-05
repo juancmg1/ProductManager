7d4ca6448c077ce3feddbd786d4e1b27c0994861
juancmg <juancmg002@gmail.com>
2024-02-05 09:37:35 -0300

    virsion 3

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import { ProductManager } from './config/ProductManager'
+const app = express()
+const PORT = 8000
+const productManager = new ProductManager('./products.json')
+
+app.get('/', (req, res) => {
+    res.send("Hola, desde mi primer servidor en Express")
+})
+
+app.get('/products', async (req, res) => {
+    const { limit } = req.query
+
+    const prods = await productManager.getProducts()
+    const limite = parseInt(limit)
+    if (limite) { 
+        if (limite < 0) {
+            res.send("Ingrese un numero valido para los queries")
+        } else {
+            const prodsLimit = prods.slice(0, limit)
+            res.send(prodsLimit)
+        }
+
+    } else {
+        res.send("Ingrese un valor valido en los Queries")
+    }
+
+
+})
+
+
+app.get('/products/:pid', async (req, res) => {
+    const idProducto = req.params.pid 
+    const prod = await productManager.getProductById(idProducto)
+    res.send(prod)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
