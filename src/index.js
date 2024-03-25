ec2c8d372864bab9d954a0239c479ae2fa17a7d2
juancmg <juancmg002@gmail.com>
2024-03-25 20:53:47 -0300

    version 10

--- src/index.js
+++ src/index.js
@@ -10,11 +10,16 @@ import mongoose from 'mongoose'
 import messageModel from './models/messages.js'
 import userRouter from './routes/userRouter.js'
 import productModel from './models/product.js'
+import cookieParser from 'cookie-parser'
+import session from 'express-session'
+import MongoStore from 'connect-mongo'
+import sessionRouter from './routes/sessionRouter.js'
+
 
 const app = express()
 const PORT = 8082
 
-mongoose.connect("mongodb+srv://juancmg002:@cluster0.azzipqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://juancmg002:juan@cluster0.azzipqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 .then(() => console.log("DB is connected"))
 .catch(e => console.log(e))
 
@@ -27,9 +32,57 @@ const io = new Server(server)
 
 //Middlewares
 app.use(express.json())
+
+app.use(session({
+    secret: "coderSecret",
+    resave: true,
+    store: MongoStore.create({
+        mongoUrl: "mongodb+srv://juancmg002:juan@cluster0.azzipqq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+        ttl: 60 * 60
+    }),
+    saveUninitialized: true
+}))
+
+app.use(cookieParser("claveSecreta"))
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + '/views')
+
+app.get('/setCookie', (req, res) => {
+    res.cookie('CookieCookie', 'Esto es una cookie :)', { maxAge: 3000000, signed: true }).send("Cookie creada") // max age es en mseg 
+})
+
+app.get('/getCookie', (req, res) => {
+    res.send(req.signedCookies)
+})
+
+app.get('/deleteCookie', (req, res) => {
+    res.clearCookie('CookieCookie').send("Cookie eliminada")
+    //res.cookie('CookieCokie', '', { expires: new Date(0) })
+})
+app.get('/session', (req, res) => {
+    console.log(req.session)
+    if (req.session.counter) {
+        req.session.counter++
+        res.send(`Sos el usuario N° ${req.session.counter} en ingresar a la pagina`)
+    } else {
+        req.session.counter = 1
+        res.send("Sos el primer usuario que ingresa a la pagina")
+    }
+})
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body
+
+    if (email == "admin@admin.com" && password == "1234") {
+        req.session.email = email
+        req.session.password = password
+
+
+    }
+    console.log(req.session)
+    res.send("Login")
+})
 
 io.on('connection', (socket) => {
     console.log("Conexion con Socket.io")
@@ -53,6 +106,7 @@ app.use('/api/products', productsRouter, express.static(__dirname + '/public'))
 app.use('/api/cart', cartRouter)
 app.use('/api/chat', chatRouter, express.static(__dirname + '/public'))
 app.use('/api/users', userRouter)
+app.use('/api/session', sessionRouter)
 
 app.post('/upload', upload.single('product'), (req, res) => {
     try {
