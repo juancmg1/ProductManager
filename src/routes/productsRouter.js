53c47c42310a4604f2985d76d9f2b8ca423f7a7d
juancmg <juancmg002@gmail.com>
2024-05-13 22:01:20 -0300

    version 14

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,66 +1,17 @@
 import { Router } from "express";
-import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js'
+import passport from "passport";
+import { getProducts, getProduct, createProduct, updateProduct, deleteProduct } from "../controllers/productsController.js";
+
 const productsRouter = Router()
 
-productsRouter.get('/', async (req, res) => {
-    try {
-        const { limit, page, filter, ord } = req.query;
-        const prods = await getProducts(limit, page, filter, ord)
+productsRouter.get('/', getProducts);
 
-        res.status(200).send(prods)
+productsRouter.get('/:pid', getProduct)
 
-    } catch (error) {
-        res.status(500).render('templates/error', {
-            error: error,
-        });
-    }
-});
+productsRouter.post('/', passport.authenticate('jwt', { session: false }), createProduct)
 
-//: significa que es modificable (puede ser un 4 como un 10 como un 75)
-productsRouter.get('/:pid', async (req, res) => {
-    try {
-        const idProducto = req.params.pid //Todo dato que se consulta desde un parametro es un string
-        const prod = await getProduct(idProducto)
-        if (prod)
-            res.status(200).send(prod)
-        else
-            res.status(404).send("Producto no existe")
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al consultar producto: ${error}`)
-    }
-})
+productsRouter.put('/:pid', passport.authenticate('jwt', { session: false }), updateProduct)
 
-productsRouter.post('/', async (req, res) => {
-    try {
-        const product = req.body
-        const mensaje = await createProduct(product)
-        res.status(201).send(mensaje)
-
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al crear producto: ${error}`)
-    }
-})
-
-productsRouter.put('/:pid', async (req, res) => {
-    try {
-        const idProducto = req.params.pid
-        const upProduct = req.body
-        const prod = await updateProduct(idProducto, upProduct)
-        res.status(200).send(prod)
-
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al actualizar producto: ${error}`)
-    }
-})
-
-productsRouter.delete('/:pid', async (req, res) => {
-    try {
-        const idProducto = req.params.pid
-        const mensaje = await deleteProduct(idProducto)
-        res.status(200).send(mensaje)
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al eliminar producto: ${error}`)
-    }
-})
+productsRouter.delete('/:pid', passport.authenticate('jwt', { session: false }), deleteProduct)
 
 export default productsRouter
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,26 +1,8 @@
 import { Router } from "express";
-import { userModel } from "../models/user.js";
+import { getUsers } from "../controllers/userController.js";
 
 const userRouter = Router()
 
-userRouter.get('/', async (req, res) => {
-    try {
-        const users = await userModel.find()
-        res.status(200).send(users)
-    } catch (e) {
-        res.status(500).send("Error al consultar users: ", e)
-    }
-})
-
-userRouter.post('/', async (req, res) => {
-    try {
-        const { nombre, apellido, email, edad, password } = req.body
-        const resultado = await userModel.create({ nombre, apellido, email, edad, password })
-        res.status(201).send(resultado)
-    } catch (e) {
-        res.status(500).send("Error al crear users: ", e)
-    }
-})
-
+userRouter.get('/', getUsers)
 
 export default userRouter
